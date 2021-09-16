8f7e008df8d2bc5f4860d19a39464bae7843e048
JoeyMBrown <brnbunch45@yahoo.com>
2021-09-16 17:29:53 -0400

    Server setup complete

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+require('dotenv').config();
+
+// const dogsRouter = require("./dogs/dogs-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// server.use('/api/dogs', dogsRouter);
+
+server.use('/', (req, res, next) => {
+    res.status(200).json({ message: "Api running!"})
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+  
+  module.exports = server;
